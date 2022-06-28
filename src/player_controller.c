1a190d0f75f8aba091bcfedde9d898f9ab85600d
zyasuo <zyasuo@student.21-school.ru>
2022-06-28 20:19:19 +0300

    player movement optimization (?)

--- src/player_controller.c
+++ src/player_controller.c
@@ -6,17 +6,27 @@
 /*   By: zyasuo <zyasuo@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 19:11:41 by zyasuo            #+#    #+#             */
-/*   Updated: 2022/06/28 19:56:01 by zyasuo           ###   ########.fr       */
+/*   Updated: 2022/06/28 20:18:39 by zyasuo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/game.h"
 
+//render_minimap(game);
+
 void	move_player(t_game *game, float y, float x)
 {
+	t_data	img;
+
+	img.img = mlx_new_image(game->window.mlx, TILE, TILE);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	render_tile(0, 0, get_color('0'), img);
+	mlx_put_image_to_window(game->window.mlx, game->window.mlx_win,
+		img.img, game->player->pos->x * TILE, game->player->pos->y * TILE);
+	mlx_destroy_image(game->window.mlx, img.img);
 	game->player->pos->x += x;
 	game->player->pos->y += y;
-	render_minimap(game);
 	render_player(game);
 }
 
